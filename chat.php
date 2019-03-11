0addb151859cd663fefcc2c502163da30abdf2c9
Romulo Alves <romuloalves81@outlook.com>
2019-03-10 23:27:54 -0300

    funcao adicionar e fechar janala

--- chat.php
+++ chat.php
@@ -12,7 +12,9 @@
    <script type="text/javascript" src="js/jquery.js"></script>-->
    <link rel="stylesheet" href="css/style.css">
    <script src="js/jquery.js"></script>
-   <script> $.noConflict(); </script>
+   <script> 
+      $.noConflict(); 
+   </script>
 </head>
 <body>
    <aside id="users_online">
@@ -28,27 +30,7 @@
    </aside>
    <!-- Janelas Chats -->
    <aside id="chats">
-      <?php for($i=1; $i<=4; $i++):?>
-         <div class="window" id="janela_x">
-            <div class="header_window"><a href="#" class="close">X</a><span class="name">Fulano de tal</span><span id="5" class="status on"></span></div>
-            <div class="body">
-               <div class="mensagens">
-                  <ul>
-                     <?php for($n=1; $n<=4; $n++):?>
-                        <li class="eu"><p>Este e um exemplo de mensagem que aparecera</p></li>
-                        <li class="">
-                           <div class="imgSmall"><img src="fotos/romulo.jpg" border="0"></div>
-                           <p>Este e um exemplo de mensagem que aparecera</p>
-                        </li>
-                     <?php endfor;?>
-                  </ul>
-               </div>
-               <div class="send_message" id="3:5">
-                  <input type="text" name="mensagem" class="msg" id="3:5">
-               </div>
-            </div>
-         </div>
-      <?php endfor;?>
+   <script src="js/functions.js"></script>   
    </aside>
 </body>
 </html> 
--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,13 @@
 jQuery(function(){
    
-   var userOnline = Number(jQuery('span.user_online').atrr('id'));
-
+   //var userOnline = Number(jQuery('span.user_online').atrr('id'));
+   // Adicionar Janela
    function add_janela(id, nome, status) {
-
       var janelas = Number(jQuery('#chats .window').length);
       var pixels = (270+5)*janelas;
       var style = 'float:none; position:absolute; bottom:0; left:'+pixels+'px';
-
       var splitDados = id.split(':');
       var id_user = Number(splitDados[1]);
-
       var janela = '<div class="window" id="janela_'+id_user+'" style="'+style+'">';
       janela += '<div class="header_window"><a href="#" class="close">X</a><span class="name">'+nome+'</span><span id="'+id_user+'" class="'+status+'"></span></div >';
       janela += '<div class="body"><div class="mensagens"><ul></ul></div>';
@@ -18,65 +15,25 @@ jQuery(function(){
 
       jQuery('#chats').append(janela);
    }
-    /* Funçao abri janela */
+   /* Funçao abri janela */
    jQuery('body').on('click', '#users_online a', function() {
       var id = jQuery(this).attr('id');
       jQuery(this).removeClass('comecar');
-
+      
       var status = jQuery(this).next().attr('class');
       var splitIds = id.split(':');
       var idJanela = Number(splitIds[1]);
 
       if(jQuery('#janela_'+idJanela).length == 0) {
-            var nome = jQuery(this).text();
-            add_janela(id, nome, status);
+      var nome = jQuery(this).text();
+      add_janela(id, nome, status);
       }else {
-            jQuery(this).removeClass('comecar');
+      jQuery(this).removeClass('comecar');
       }
    });
-   /* Funçao minimizar janela */
-   jQuery('body').on('click', '.header_window', function() {
-      var next = jQuery(this).next();
-      next.toggle(100);
-   });
-    /* Funçao fechar janela */
-   jQuery('body').on('click', '.close', function() {
-      var parent = jQuery(this).parent().parent();
-      var idParent = parent.attr('id');
-      var splitParent = idParent.split('_');
-      var idJanelaFechada = Number(splitParent[1]);
 
-      var contagem = Number(jQuery('.window').length)-1;
-      var indice = Number(jQuery('.close').index(this));
-      var restamAFrente = contagem-indice;
 
-      for(var i =1; i <= restamAFrente; i++) {
-         jQuery('.window:eq('+(indice+i)+')').animate({left:"-=275"}, 200);
-      }
-      parent.remove();
-      jQuery('#users_online li#'+idJanelaFechada+' a').addClass('comecar');
-   });
-   /* Funçao fechar janela */
-   jQuery('body').on('keyup', '.msg', function(e) {
-      if(e.which == 13) {
-         var texto = jQuery(this).val();
-          var id = jQuery(this).attr('id');
-       var split = id.split(':');
-        var para = Number(split[1]);
-         
-         jQuery.ajax({
-            type: 'POST',
-            url: 'sys/submit.php',
-            data: {mensagem: texto, de: userOnline, para: para},
-            success: function(retorno) {
-              if(retorno == 'ok') {
-                 jQuery('.msg').val('');
-              }else {
-                 alert("Ocorreu um erro ao enviar a mensagem");
-               }
-            }
-        });
-       }
-   });
+
+
 
 });
